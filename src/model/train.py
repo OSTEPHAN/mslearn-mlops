2a6d7a5bc253a4740cb27761328123aa200f5b0c
OSTEPHAN <olivier.stephan@live.fr>
2023-07-19 18:54:57 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -57,7 +57,10 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(
+        C=1 / reg_rate,
+        solver="liblinear"
+    ).fit(X_train, y_train)
 
 
 def parse_args():
@@ -65,8 +68,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest="training_data", type=str)
-    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
+    parser.add_argument("--training_data",
+                        dest="training_data", type=str)
+    parser.add_argument("--reg_rate",
+                        dest="reg_rate", type=float,
+                        default=0.01)
 
     # parse args
     args = parser.parse_args()
